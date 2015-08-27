100f8eaee4d199de6161eaee9ed0721bc2868320
XD.Yang <xd.yang@nf-3.com>
2015-08-27 21:03:21 +0800

    init project

---
+++ ios/main.cpp
@@ -0,0 +1,27 @@
+﻿#include "navigatelib.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    PointArray pt =   loadPathInfo("gemo.bin");
+
+    NavPoint start,end;
+
+    start.x = 375;
+    start.y = 1005;
+
+    end.x = 375;
+    end.y = 405;
+
+    pt = getBestPath(&start,&end);
+
+    for ( int i = 0 ; i < pt.num; ++i)
+    {
+        cout<<pt.pts[i].id<<endl;
+    }
+
+    return 0;
+}
---
+++ jni/navigatelib.h
@@ -0,0 +1,47 @@
+﻿#ifndef NAVIGATELIB_H
+#define NAVIGATELIB_H
+
+
+typedef double real;
+
+typedef struct _Point
+{
+    real    x;
+    real    y;
+    int     attr;
+    int     id;
+    int     type;
+}NavPoint;
+
+typedef struct
+{
+    int     num;
+    NavPoint  *pts;
+}PointArray;
+
+class Navigate;
+
+extern "C"
+{
+    ///
+    /// 加载路径文件
+    ///
+    /// 返回路径点数组
+    ///
+    PointArray      loadPathInfo(const char *filepath);
+
+    ///
+    /// \brief getBestPath
+    ///
+    /// 根据给定的起点和终点进行路径规划
+    /// \param start
+    /// \param end
+    /// \return
+    ///
+    ///
+    PointArray      getBestPath(NavPoint *start, NavPoint *end);
+
+}
+
+
+#endif // NAVIGATELIB_H
