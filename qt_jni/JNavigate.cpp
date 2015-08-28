bac9927a4e297dd153d5824db70adae4926f0709
lorinx8 <lorinx8@gmail.com>
2015-08-28 16:54:21 +0800

    fix the jni local reference table overflow problem

--- qt_jni/JNavigate.cpp
+++ qt_jni/JNavigate.cpp
@@ -1,111 +1,125 @@
-﻿
-//#include "defines.h"
+﻿//#include "defines.h"
 //#include "navigate.h"
-#include "navigate_Navigate.h"
-
 #include "navigatelib.h"
+#include "navigate_Navigate.h"
+#include "jlog.h"
 
-//static Navigate global_nav;
+// static Navigate global_nav;
 
-JNIEXPORT jobjectArray JNICALL Java_navigate_Navigate_loadPathInfo
-  (JNIEnv *env, jobject, jstring filename)
-{
-    jboolean bcopy = JNI_FALSE;
-    const char *fname = (env)->GetStringUTFChars(filename,&bcopy);
-    //global_nav.LoadPointsFile(fname);
-    PointArray res = loadPathInfo(fname);
+JNIEXPORT jint JNICALL Java_me_bukp_pakamaplib_business_PakaCoreJniBridge_just4T(
+		JNIEnv *env, jobject) {
+	int t = 1;
+	return (jint) t;
+}
 
-    //vector<Node*> points = global_nav.GetAllPoints();
+JNIEXPORT jobjectArray JNICALL Java_me_bukp_pakamaplib_business_PakaCoreJniBridge_loadPathInfo(
+		JNIEnv *env, jobject, jstring filename) {
+	jboolean bcopy = JNI_FALSE;
+	const char *fname = (env)->GetStringUTFChars(filename, &bcopy);
+	//global_nav.LoadPointsFile(fname);
+	PointArray res = loadPathInfo(fname);
 
-    int  pointnum = res.num;
+	//vector<Node*> points = global_nav.GetAllPoints();
 
-    jobjectArray    pointArray;
+	int pointnum = res.num;
 
-    jclass objclass = (env)->FindClass("java/lang/Object");
-    //新建object数组
-    pointArray = (env)->NewObjectArray(pointnum,objclass,0);
+	jobjectArray pointArray;
 
-    //获取java中的实例类
-    jclass pointclass = (env)->FindClass("navigate/Point");
+	jclass objclass = (env)->FindClass("java/lang/Object");
+	//新建object数组
+	pointArray = (env)->NewObjectArray(pointnum, objclass, 0);
 
-    //ToDo : add id attribute of the point
-    jfieldID xid = (env)->GetFieldID(pointclass,"x","F");
-    jfieldID yid = (env)->GetFieldID(pointclass,"y","F");
-    jfieldID attrid = (env)->GetFieldID(pointclass,"attr","I");
-    jfieldID type = (env)->GetFieldID(pointclass,"type","I");
+	//获取java中的实例类
+	jclass pointclass = (env)->FindClass("me/bukp/pakamaplib/business/Point");
 
+	//ToDo : add id attribute of the point
+	jfieldID xid = (env)->GetFieldID(pointclass, "x", "F");
+	jfieldID yid = (env)->GetFieldID(pointclass, "y", "F");
+	jfieldID attrid = (env)->GetFieldID(pointclass, "attr", "I");
+	jfieldID type = (env)->GetFieldID(pointclass, "type", "I");
 
-    for ( int i = 0 ; i < pointnum; ++i)
-    {
-        jobject point  = (env)->AllocObject(pointclass);
-        (env)->SetFloatField(point,xid,res.pts[i].x);
-        (env)->SetFloatField(point,yid,res.pts[i].y);
-        (env)->SetIntField(point,attrid,res.pts[i].attr);
-        (env)->SetIntField(point,type,res.pts[i].type);
-        (env)->SetObjectArrayElement(pointArray,i,point);
-    }
-    delete res.pts;
-    res.pts = NULL;
-    return pointArray;
+	for (int i = 0; i < pointnum; ++i) {
+		jobject point = (env)->AllocObject(pointclass);
+		(env)->SetFloatField(point, xid, res.pts[i].x);
+		(env)->SetFloatField(point, yid, res.pts[i].y);
+		(env)->SetIntField(point, attrid, res.pts[i].attr);
+		(env)->SetIntField(point, type, res.pts[i].type);
+		(env)->SetObjectArrayElement(pointArray, i, point);
+		(env)->DeleteLocalRef(point);
+	}
+
+	(env)->DeleteLocalRef(pointclass);
+	(env)->DeleteLocalRef(objclass);
+	(env)->ReleaseStringUTFChars(filename, fname);
+	delete res.pts;
+	res.pts = 0;
+	return pointArray;
 
 }
 
+JNIEXPORT jobjectArray JNICALL Java_me_bukp_pakamaplib_business_PakaCoreJniBridge_getBestPath(
+		JNIEnv *env, jobject, jobject start, jobject end) {
+	LOGD("ENTER");
+	jobjectArray resultPath;
+	//static Node nstart,nend;
+	static NavPoint nstart, nend;
+	LOGD("11111");
+	//获取起点和终点
+	jclass pointClass = (env)->FindClass("me/bukp/pakamaplib/business/Point");
 
-JNIEXPORT jobjectArray JNICALL Java_navigate_Navigate_getBestPath
-  (JNIEnv *env, jobject, jobject start, jobject end)
+	jfieldID idId = (env)->GetFieldID(pointClass, "id", "I");
+	jfieldID xid = (env)->GetFieldID(pointClass, "x", "F");
+	jfieldID yid = (env)->GetFieldID(pointClass, "y", "F");
+	jfieldID attrId = (env)->GetFieldID(pointClass, "attr", "I");
+	jfieldID type = (env)->GetFieldID(pointClass, "type", "I");
 
-{
-    jobjectArray    resultPath = 0;
+	int sid = (env)->GetIntField(start, idId);
+	int eid = (env)->GetIntField(end, idId);
+	float fsx = (env)->GetFloatField(start, xid);
+	float fsy = (env)->GetFloatField(start, yid);
+	float fex = (env)->GetFloatField(end, xid);
+	float fey = (env)->GetFloatField(end, yid);
+	int sattr = (env)->GetIntField(start, attrId);
+	int eattr = (env)->GetIntField(end, attrId);
 
-    //static Node nstart,nend;
-    static NavPoint nstart,nend;
-    //获取起点和终点
-    jclass pointClass = (env)->FindClass("navigate/Point");
+	nstart.id = sid;
+	nstart.x = fsx;
+	nstart.y = fsy;
+	nstart.attr = sattr;
 
-    jfieldID  xid = (env)->GetFieldID(pointClass,"x","F");
-    jfieldID  yid = (env)->GetFieldID(pointClass,"y","F");
-    jfieldID  attrId = (env)->GetFieldID(pointClass,"attr","I");
-    jfieldID  type = (env)->GetFieldID(pointClass,"type","I");
+	nend.id = eid;
+	nend.x = fex;
+	nend.y = fey;
+	nend.attr = eattr;
+	LOGD("22222, start %d-%f-%f-%d, end %d-%f-%f-%d", sid, fsx, fsy, sattr, eid,
+			fex, fey, eattr);
+	PointArray res = getBestPath(&nstart, &nend);
+	LOGD("33333");
+	jclass objclass = (env)->FindClass("java/lang/Object");
 
-    float fsx = (env)->GetFloatField(start,xid),
-          fsy = (env)->GetFloatField(start,yid),
-          fex = (env)->GetFloatField(end,xid),
-          fey = (env)->GetFloatField(end,yid);
-    int   sattr = (env)->GetIntField(start,attrId),
-          eattr = (env)->GetIntField(end,attrId);
+	resultPath = (env)->NewObjectArray(res.num, objclass, 0);
 
-    nstart.x = fsx;
-    nstart.y = fsy;
-    nstart.attr = sattr;
+	//list<Node*>::iterator it = res.begin();
+	int count = res.num;
+	LOGD("44444 %d", count);
+	int i = 0;
+	while (i < count) {
+		jobject node = (env)->AllocObject(pointClass);
 
-    nend.x = fex;
-    nend.y = fey;
-    nend.attr = eattr;
-
-    //list<Node*> res = global_nav.GetBestPath(&nstart,&nend);
-
-    PointArray res = getBestPath(&nstart,&nend);
-
-    jclass objclass = (env)->FindClass("java/lang/Object");
-    resultPath = (env)->NewObjectArray(res.num,objclass,0);
-
-    //list<Node*>::iterator it = res.begin();
-    int count = res.num;
-
-    int i = 0;
-    while ( i < count )
-    {
-        jobject node = (env)->AllocObject(pointClass);
-
-        (env)->SetFloatField(node,xid,res.pts[i].x);
-        (env)->SetFloatField(node,yid,res.pts[i].y);
-        (env)->SetIntField(node,attrId,res.pts[i].attr);
-        (env)->SetIntField(node,type,res.pts[i].type);
-        (env)->SetObjectArrayElement(resultPath,i,node);
-        ++i;
-    }
-    delete res.pts;
-    res.pts = NULL;
-    return resultPath;
+		(env)->SetIntField(node, idId, res.pts[i].id);
+		(env)->SetFloatField(node, xid, res.pts[i].x);
+		(env)->SetFloatField(node, yid, res.pts[i].y);
+		(env)->SetIntField(node, attrId, res.pts[i].attr);
+		(env)->SetIntField(node, type, res.pts[i].type);
+		(env)->SetObjectArrayElement(resultPath, i, node);
+		(env)->DeleteLocalRef(node);
+		++i;
+	}
+	LOGD("55555");
+	(env)->DeleteLocalRef(pointClass);
+	(env)->DeleteLocalRef(objclass);
+	delete res.pts;
+	res.pts = 0;
+	return resultPath;
 
 }
