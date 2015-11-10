28dd4cf53e70d2285e2296d88485594d0971a47e
Seven-Yang <xd.yang@nf-3.com>
2015-11-10 23:54:04 +0800

    新增导航异常类

---
+++ src/navexception.hpp
@@ -0,0 +1,30 @@
+﻿#ifndef NAVEXCEPTION_H
+#define NAVEXCEPTION_H
+
+#include <exception>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+const int MaxMsgLen = 128;
+
+class NavException:public exception
+{
+public:
+
+    NavException():exception()
+    {
+    }
+
+    NavException(const char *m):exception( m )
+    {
+    }
+
+    ~NavException()
+    {
+
+    }
+};
+
+#endif // NAVEXCEPTION_H
